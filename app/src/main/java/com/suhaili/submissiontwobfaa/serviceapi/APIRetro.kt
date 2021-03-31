432d2f2c43b57d67f2825ff995e677a81517f0b0
Suhaili Faruq <suhaii.faruq01@gmail.com>
2021-04-01 01:25:10 +0700

    Change token API because delete from my github

--- app/src/main/java/com/suhaili/submissiontwobfaa/serviceapi/APIRetro.kt
+++ app/src/main/java/com/suhaili/submissiontwobfaa/serviceapi/APIRetro.kt
@@ -8,27 +8,27 @@ import retrofit2.http.*
 
 interface APIRetro {
     @GET("users")
-    @Headers("Authorization: token 39aeb15debbadccadf1a8132ef04997cc4b6b724")
+    @Headers("Authorization: token d8dc3faf2edb4a4a85f5d70ff8cceafba9a0618f")
     fun getAllData(): Call<ArrayList<LoginModel>>
 
 
     @GET("users/{login}")
-    @Headers("Authorization: token 39aeb15debbadccadf1a8132ef04997cc4b6b724")
+    @Headers("Authorization: token d8dc3faf2edb4a4a85f5d70ff8cceafba9a0618f")
     fun getUserData(@Path("login") login: String): Call<GitModel>
 
 
     @GET("users/{login}/following")
-    @Headers("Authorization: token 39aeb15debbadccadf1a8132ef04997cc4b6b724")
+    @Headers("Authorization: token d8dc3faf2edb4a4a85f5d70ff8cceafba9a0618f")
     fun getFollowing(@Path("login") login: String): Call<ArrayList<LoginModel>>
 
 
     @GET("users/{login}/followers")
-    @Headers("Authorization: token 39aeb15debbadccadf1a8132ef04997cc4b6b724")
+    @Headers("Authorization: token d8dc3faf2edb4a4a85f5d70ff8cceafba9a0618f")
     fun getFollower(@Path("login") login: String): Call<ArrayList<LoginModel>>
 
 
     @GET("search/users")
-    @Headers("Authorization: token 39aeb15debbadccadf1a8132ef04997cc4b6b724")
+    @Headers("Authorization: token d8dc3faf2edb4a4a85f5d70ff8cceafba9a0618f")
     fun getFindPeople(@Query("q") q: String): Call<FindModel>
 
 }
